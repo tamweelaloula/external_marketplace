7d4815a1cb7db4bd50e825abb6901764875f61f6
sohail <sbhatti@sunbonn.com>
2026-01-05 19:37:05 +0500

    base url updated

--- src/lib/services/nafathApi.ts
+++ src/lib/services/nafathApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { convertToHijri, formatDateToDDMMYYYY, normalizeDob } from "@/lib/utils";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL_SIT
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export const nafathApi = createApi({
   reducerPath: "nafathApi",
